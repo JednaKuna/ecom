8932655dfa2403f6e5f6e3fb4293514288199502
JednaKuna <karlo.ujdenica29@gmail.com>
2024-12-10 11:44:31 +0100

    Added cart and sessions

---
+++ ecom/cart/cart.py
@@ -0,0 +1,46 @@
+from store.models import Product
+
+class Cart():
+    def __init__(self, request):
+        self.session = request.session
+        
+        # Get the current session key if it exists
+        cart = self.session.get('session_key')
+        
+        # If the user is now, no session key. Create one.
+        if 'session_key' not in request.session:
+            cart = self.session['session_key'] = {}
+            
+        # Make sure cart works on all sites
+        self.cart = cart
+        
+    def add(self, product, quantity):
+        # Get values
+        product_id = str(product.id)
+        product_qty = str(quantity)
+        # Logic
+        if product_id in self.cart:
+            pass
+        else:
+            #self.cart[product_id] = {'price': str(product.price)}
+            self.cart[product_id] = int(product_qty)
+            
+        self.session.modified = True
+        
+    def __len__(self):
+        return len(self.cart)
+    
+    def get_prods(self):
+        # Get Ids 
+        product_ids = self.cart.keys()
+        
+        # Use ids to look up products
+        products = Product.objects.filter(id__in=product_ids)
+        
+        return products
+    
+    def get_quants(self):
+        quantities = self.cart
+        
+        return quantities
+        
---
+++ ecom/cart/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
+
+def cart_summary(request):
+    # Get the cart
+    cart = Cart(request)
+    cart_products = cart.get_prods
+    quantities = cart.get_quants
+    return render(request, "cart_summary.html", {"cart_products":cart_products, "quantities":quantities})
+
+def cart_add(request):
+    # Get the cart
+    cart = Cart(request)
+    # Test for POST
+    if request.POST.get('action') == 'post':
+        
+        # Get stuff
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
+        
+        # Lookup product in Database
+        product = get_object_or_404(Product, id=product_id)
+        
+        # Save to a session
+        cart.add(product=product, quantity=product_qty)
+        
+        # Return response
+        #response = JsonResponse({'Product Name': product.name})
+        
+        # Get Cart Quantity
+        cart_quantity = cart.__len__()
+        
+        response = JsonResponse({'qty': cart_quantity})
+        
+        return response
+         
+
+def cart_delete(request):
+    pass
+
+def cart_update(request):
+    pass
